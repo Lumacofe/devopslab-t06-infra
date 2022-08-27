1b71e5bf171820f7021b0dac808c36f6bac8ffa9
Lumacofe <42089105+Lumacofe@users.noreply.github.com>
2022-08-27 13:00:30 -0300

    Terraform Config

--- gcp-main.tf
+++ gcp-main.tf
@@ -2,9 +2,9 @@
 resource "google_compute_instance" "firstvm" {
   name         = "helloworld"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
+  zone         = "us-east4-c"
 
-  # Defini a Imagem da VM
+  # Define a Imagem da VM
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20220303a"
