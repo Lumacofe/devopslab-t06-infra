84fea33855233990efadf4b0577e3aafa776ed6c
Lumacofe <42089105+Lumacofe@users.noreply.github.com>
2022-08-27 13:59:26 -0300

    Fix Credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/luis_feliz/gcpkey.json")
-
   project = "divine-ceremony-360015"
   region  = "us-east4"
   zone    = "us-east4-c"
